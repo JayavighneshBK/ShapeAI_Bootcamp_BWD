5946defa06574aae0c3350be57ee3cbee43f3d9e
JayavighneshBK <86564993+JayavighneshBK@users.noreply.github.com>
2021-06-27 21:22:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and React JS</h1>
+      <p>A basic Javascript and React JS Bootcamp from Scratch</p>
+    </div>
+  );
+}
+export default Info;
